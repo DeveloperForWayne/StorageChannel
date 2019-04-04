22ca12f803ee38461922c8e829e93da55c321af1
Wayne.Xu <wayne.xu.work@gmail.com>
2019-04-04 17:05:41 -0400

    coverage fixed

---
+++ .solcover.js
@@ -0,0 +1,4 @@
+module.exports = {
+    copyPackages: ['openzeppelin-solidity'],
+    testrpcOptions: '--mnemonic="assist swim enroll illness settle toilet trade short uncover era feature planet" --port 8555'
+};
--- truffle-config.js
+++ truffle-config.js
@@ -64,5 +64,12 @@ module.exports = {
       network_id: "*",
     }
   },
+  coverage: {
+    host: "127.0.0.1",
+    network_id: "*",
+    port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+    gas: 0xfffffffffff, // <-- Use this high gas value
+    gasPrice: 0x01      // <-- Use this low gas price
+  },
   plugins: [ "truffle-security" ]
 };
